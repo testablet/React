3a4baefa2dc3378c43fe8793ecf5857c26e415da
testablet <teddyestablet84@gmail.com>
2024-04-22 14:17:57 +0200

    Add navbar, add home & edit app

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Home />
+          {/*<About />*/}
+          {/*<Projects />*/}
+          {/*<Contact />*/}
+        </main>
+        <footer>
+          <p>Copyright © 2024 - Tous droits réservés</p>
+        </footer>
+      </div>
   );
 }
 
